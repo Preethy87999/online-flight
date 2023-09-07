42ea06f11897fe6b28cfd2f0416b073a95ca658f
Preethy87999 <124979741+Preethy87999@users.noreply.github.com>
2023-09-07 16:34:41 +0530

    Update ApiTesting.java

--- src/test/java/Test/ApiTesting.java
+++ src/test/java/Test/ApiTesting.java
@@ -15,13 +15,13 @@ public class ApiTesting {
 		
 		String url = "http://localhost:8081/FlyAway/";
 		RestAssured.baseURI = url;
-		RestAssured.given().get("adminbookings").then().log().body().statusCode(200);
+		RestAssured.given().get("adminbookings?id=1").then().log().body().statusCode(200);
 	}
 	@Test
 	public void getBookSeat() {
 		 RequestSpecification httpRequest = RestAssured.given();
 
-		Response response = httpRequest.get("http://localhost:8081/FlyAway/");
+		Response response = httpRequest.get("http://localhost:8081/FlyAway/memberbookings");
 		response.then().log().status()
 	      .statusCode(200);		 
 	}
